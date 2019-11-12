b9fac0dd6e762133475ff0cf962246999526e2ee
unknown <mikaelldavi@gmail.com>
2019-11-12 20:03:21 -0300

    Fim Métodos Usuario

--- ProjetoCRUDADS/src/univs/edu/usuario/Usuario.java
+++ ProjetoCRUDADS/src/univs/edu/usuario/Usuario.java
@@ -1,72 +1,57 @@
-
 package univs.edu.usuario;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+
+@Entity
 public class Usuario {
     
+    @Id
+    @GeneratedValue
     private int idUsuario;
     
+    @Column(length = 100, nullable = false)
     private String nomeUsuario;
     
+    @Column(length = 100, nullable = false, unique = true)
     private String login;
     
+    @Column(length = 100, nullable = false)
     private String senha;
 
-    /**
-     * @return the idUsuario
-     */
     public int getIdUsuario() {
         return idUsuario;
     }
 
-    /**
-     * @param idUsuario the idUsuario to set
-     */
     public void setIdUsuario(int idUsuario) {
         this.idUsuario = idUsuario;
     }
 
-    /**
-     * @return the nomeUsuario
-     */
     public String getNomeUsuario() {
         return nomeUsuario;
     }
 
-    /**
-     * @param nomeUsuario the nomeUsuario to set
-     */
     public void setNomeUsuario(String nomeUsuario) {
         this.nomeUsuario = nomeUsuario;
     }
 
-    /**
-     * @return the login
-     */
     public String getLogin() {
         return login;
     }
 
-    /**
-     * @param login the login to set
-     */
     public void setLogin(String login) {
         this.login = login;
     }
 
-    /**
-     * @return the senha
-     */
     public String getSenha() {
         return senha;
     }
 
-    /**
-     * @param senha the senha to set
-     */
     public void setSenha(String senha) {
         this.senha = senha;
     }
-
-
+    
 }
